ed2a6aec86517a5f210abd21771174bcff5d29ba
Akshat Jain <161316297+jain17akshat@users.noreply.github.com>
2025-03-31 06:21:13 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-const express=require("express")
+import express from 'express'
+const app=express();
+app.get('/',(req,res)=>{
+    res.send("Server is ready")
+});
+
+const port =process.env.PORT||3000
